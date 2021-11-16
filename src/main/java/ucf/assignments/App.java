b9c00af3614598b59a9566537fa48cb49ca6ec66
ajdahcode <aj.bagarra@gmail.com>
2021-11-15 20:07:45 -0500

    work

--- src/main/java/ucf/assignments/App.java
+++ src/main/java/ucf/assignments/App.java
@@ -13,67 +13,19 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class App extends Application {
-    /*
-    Start of Notes:
-
-    void list{
-        while (list hasn't been created){
-            create a max 100 unique to-do list
-
-            title function for title of to-do list;
-
-            for every to-do list{
-                max 100 items
-                item_description for item
-            }
-        }
+    // Launch Application
+    public static void main(String[] args) {
+        launch();
     }
 
-    public title (input){
-        should take in a title given by input
-        returns the title
-    }
-
-    public item_description(description){
-        input of the due date: (YYYY-MM-DD)
-        string of description with due-date;
-        returns the string of description
-    }
-
-
-    void process{
-//        this function will have the user inputs where we can
-//        add, remove, edit, mark, display, save, and load
-//        suggested by the assignment
-
-        if the user wants to add{
-            add the item into list
-        }
-        else (the user wants to delete){
-            delete the item from list
-        }
-        if the user wants to mark and display{
-            mark and display the item from the list
-        }
-        if the user wants to save{
-            save the items from list
-        }
-        if the user wants to load{
-            load the items into list
-        }
-    }
-
-     */
+    // Creates the first stage of the Application
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/as4.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/startList.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("To-Do List");
         stage.setScene(scene);
         stage.show();
     }
 
-    public static void main(String[] args) {
-        launch();
-    }
 }
--- src/main/java/ucf/assignments/AppController.java
+++ src/main/java/ucf/assignments/AppController.java
@@ -1,14 +1,36 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 alden bagarra
+ */
+
 package ucf.assignments;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
 
 public class AppController {
     @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void onButtonClick(ActionEvent actionEvent) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/makeList.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
+
+    public void onSavedLists(ActionEvent actionEvent) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/savedLists.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
